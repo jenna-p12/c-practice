75ff390afaf5dbd2ebc3142586f715e21fe39a5f
jenna-p12 <159454346+jenna-p12@users.noreply.github.com>
2024-09-10 10:41:11 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a = 5;
+    float b = 1.25;
+    char c = 'j';
+    bool d = false;
+    string e = "hello";
+    cout << "An example of an integer is: " << a << endl;
+    cout << "An example of a float is: " << b << endl;
+    cout << "An example of a char is: " << c << endl;
+    cout << "An example of a boolean is: " << d << endl;
+    cout << "An example of an string is: " << e << endl;
+
+}
